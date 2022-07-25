ad5215d3644cde5a5a0f9fe25505c5dc3018c78c
Miracle Iloh <ilohmiracleugo@gmail.com>
2022-07-25 14:54:43 +0000

    updated kubernetes script

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,20 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="ugoiloh/prediction"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run predictionapp\
+    --image=$dockerpath\
+    --port=80 --labels app=prediction
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward predictionapp 8000:80
 
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="ugoiloh/prediction"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag prediction $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
